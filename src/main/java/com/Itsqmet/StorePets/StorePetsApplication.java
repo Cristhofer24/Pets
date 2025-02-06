217d8106ee5bb032b85935b607e2d9f9c913f005
cristhofer24 <you@example.com>
2025-02-05 20:55:29 -0500

    Tienda de mascotas

---
+++ src/main/java/com/Itsqmet/StorePets/StorePetsApplication.java
@@ -0,0 +1,13 @@
+package com.Itsqmet.StorePets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorePetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Itsqmet/StorePets/StorePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Itsqmet.StorePets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
