6eaf86f5f2baf6f7b93f8d4037dff8a14bc86308
Santos-de-Israel <144865373+Santos-de-Israel@users.noreply.github.com>
2025-02-06 23:56:29 -0500

    cambios en el frond- end

--- src/main/java/com/Itsqmet/StorePets/Controlador/MainController.java
+++ src/main/java/com/Itsqmet/StorePets/Controlador/MainController.java
@@ -3,8 +3,7 @@ package com.Itsqmet.StorePets.Controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
